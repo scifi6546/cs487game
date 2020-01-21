18e4357d506e74d291845b7912240d61dab059a9
Jonathan Metzgar <microwerx@outlook.com>
2020-01-20 15:26:29 -0900

    JBM: fix namespace name, put structs in namespace

--- gamelib/gamelib_base.hpp
+++ gamelib/gamelib_base.hpp
@@ -21,56 +21,57 @@
 #include <memory>
 #include <thread>
 
-struct SPRITEINFO {
-    glm::vec2 position;
-    float angle;
-    glm::vec2 center;
-    int flipFlags;
-};
+namespace GameLib {
+    struct SPRITEINFO {
+        glm::vec2 position;
+        float angle;
+        glm::vec2 center;
+        int flipFlags;
+    };
 
-struct TILEIMAGE {
-    SDL_Texture* texture{ nullptr };
-    int tileId{ 0 };
-    int tilesetId{ 0 };
-    int w{ 0 };
-    int h{ 0 };
-};
+    struct TILEIMAGE {
+        SDL_Texture* texture{ nullptr };
+        int tileId{ 0 };
+        int tilesetId{ 0 };
+        int w{ 0 };
+        int h{ 0 };
+    };
 
-struct AUDIOINFO {
-    Mix_Chunk* chunk{ nullptr };
-    std::string name;
+    struct AUDIOINFO {
+        Mix_Chunk* chunk{ nullptr };
+        std::string name;
 
-    operator bool() const { return chunk != nullptr; }
+        operator bool() const { return chunk != nullptr; }
 
-    void free() {
-        Mix_FreeChunk(chunk);
-        chunk = nullptr;
-    }
-
-    ~AUDIOINFO() {
-        if (chunk) {
-            HFLOGERROR("Should not delete sound in destructor");
+        void free() {
+            Mix_FreeChunk(chunk);
+            chunk = nullptr;
         }
-    }
-};
 
-struct MUSICINFO {
-    Mix_Music* chunk{ nullptr };
-    std::string name;
-
-    operator bool() const { return chunk != nullptr; }
-
-    void free() {
-        Mix_FreeMusic(chunk);
-        chunk = nullptr;
-    }
-
-    ~MUSICINFO() {
-        if (chunk) {
-            HFLOGERROR("Should not delete sound in destructor");
+        ~AUDIOINFO() {
+            if (chunk) {
+                HFLOGERROR("Should not delete sound in destructor");
+            }
         }
-    }
-};
+    };
 
+    struct MUSICINFO {
+        Mix_Music* chunk{ nullptr };
+        std::string name;
+
+        operator bool() const { return chunk != nullptr; }
+
+        void free() {
+            Mix_FreeMusic(chunk);
+            chunk = nullptr;
+        }
+
+        ~MUSICINFO() {
+            if (chunk) {
+                HFLOGERROR("Should not delete sound in destructor");
+            }
+        }
+    };
+}
 
 #endif
